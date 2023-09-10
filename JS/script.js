702d68ae9265ea2830b64cbb6246d63730bb8f07
Amaiyo Praises <amaiyo.praises@gmail.com>
2023-09-10 22:01:48 +0100

    added card and  circular progress indicator ffor client services used

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,9 @@
-// script.js
-const progress = document.querySelector('.progress');
-const percentageText = document.querySelector('.percentage');
+function updateProgress(progressId, percentage) {
+  const progressCircle = document.getElementById(progressId);
+  const progress = progressCircle.querySelector('.progress');
+  const percentageText = progressCircle.querySelector('.percentage');
 
-function updateProgress(percentage) {
-  const circumference = 2 * Math.PI * 45; // Circumference of the circle
+  const circumference = 2 * Math.PI * 45;
   const offset = circumference - (percentage / 100) * circumference;
 
   progress.style.strokeDasharray = `${circumference} ${circumference}`;
@@ -11,5 +11,13 @@ function updateProgress(percentage) {
   percentageText.textContent = `${percentage}%`;
 }
 
-// Example: Set progress to 75%
-updateProgress(75);
+// Note that if it is 4/5 then calculate 4/5 * 100 and input the value
+
+// Update progress for domain
+updateProgress('domain', 75);
+
+// Update progress for alias
+updateProgress('alias', 100);
+
+// Update progress for domainCreated
+updateProgress('domainCreated', 25);
